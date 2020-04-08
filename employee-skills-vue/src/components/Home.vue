32984d247355a55f501815d0f38da7e9052fa52e
Christina Caruso <ChristinaCaruso57@gmail.com>
2020-04-08 10:06:34 -0400

    added home page logo

--- employee-skills-vue/src/components/Home.vue
+++ employee-skills-vue/src/components/Home.vue
@@ -1,25 +1,12 @@
 <template>
-<div>
-      <div class='employee-info' v-for='employee in allEmployees' :key='employee.id'>
-
-       Employee First Name: {{employee.firstName}} <br>
-        Employee Last Name: {{employee.lastName}} <br>
-        Employee Email: {{employee.companyEmail}} <br>
-        Employee Date of Birth: {{employee.birthDate}} <br>
-        Employee Hire Date: {{employee.hireDate}} <br>
-        Role:  {{employee.role}} <br>
-        
-
-        <router-link :to="{name: 'display-detail', params: {id: employee.id}}"> update Employee </router-link><br><br>
-    
-    </div>
-</div>
+<div id="app">
+    <img :src= "require(`@/img/perficientlogo.png`)" class = "header-image" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
- 
- 
+
 }
 </script>
